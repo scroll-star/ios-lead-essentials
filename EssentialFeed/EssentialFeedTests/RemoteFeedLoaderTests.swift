6ab4a13320530456e05f44ff42787e610ab451a1
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-05-22 17:09:27 +0300

    Inject URL instead hardcoding it; Make HTTP client abstraction; Create private extension which will be used to accommodate helpers

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,34 +8,62 @@
 import XCTest
 
 final class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        client.get(from: url)
     }
 }
 
-final class HTTPClient {
-    static let shared = HTTPClient()
-    
-    private init() {}
-    
-    var requestedURL: URL?
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
+    private var client: HTTPClientSpy!
+    
+    override func setUp() {
+        super.setUp()
+        client = .init()
+    }
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
-        _ = RemoteFeedLoader()
+        let url = URL(string: "https://some-url.com")!
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
-        let sut = RemoteFeedLoader()
+        let url = URL(string: "https://some-url.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
+    }
+}
+
+// MARK: Utils
+
+private extension RemoteFeedLoaderTests {
+    func makeSUT(url: URL = URL(string: "https://some-url.com")!) -> RemoteFeedLoader {
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return sut
+    }
+    
+    class HTTPClientSpy: HTTPClient {
+        func get(from url: URL) {
+            requestedURL = url
+        }
+        var requestedURL: URL?
     }
 }
