65459c3f77837d3367e3f5aeedc03db71a4b5853
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-05-30 18:58:24 +0300

    Add possibility to stub response data and `HTTPURLResponse`

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -31,7 +31,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
-        URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
         
         let sut = URLSessionHTTPClient()
         
@@ -63,11 +63,18 @@ private extension URLSessionHTTPClientTests {
         private static var stubs = [URL: Stub]()
         
         private struct Stub {
+            let data: Data?
+            let response: URLResponse?
             let error: Error?
         }
         
-        static func stub(url: URL, error: Error? = nil) {
-            stubs[url] = Stub(error: error)
+        static func stub(
+            url: URL,
+            data: Data?,
+            response: HTTPURLResponse?,
+            error: Error? = nil
+        ) {
+            stubs[url] = Stub(data: data, response: response, error: error)
         }
         
         static func startInterceptingRequests() {
@@ -92,10 +99,18 @@ private extension URLSessionHTTPClientTests {
         override func startLoading() {
             guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
             
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
             }
-            
+
             client?.urlProtocolDidFinishLoading(self)
         }
         
