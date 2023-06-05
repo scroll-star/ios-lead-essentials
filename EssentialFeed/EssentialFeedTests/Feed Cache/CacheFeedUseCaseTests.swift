65c93a185b4c6726c80e2762ab40b8284d011aa3
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-05 18:02:33 +0300

    Save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,23 +6,56 @@
 //
 
 import XCTest
+import EssentialFeed
 
 final class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 final class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
-
+    
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
-
+        
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+}
+
+// MARK: - Helpers
+
+private extension CacheFeedUseCaseTests {
+    func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
