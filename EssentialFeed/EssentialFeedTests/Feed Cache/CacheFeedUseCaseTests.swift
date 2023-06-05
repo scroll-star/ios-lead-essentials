367a412604616a60ef93aa0bbf9833e2d798ef6d
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-05 18:07:09 +0300

    Track for memory leaks

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -49,9 +49,11 @@ final class CacheFeedUseCaseTests: XCTestCase {
 // MARK: - Helpers
 
 private extension CacheFeedUseCaseTests {
-    func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
 
