133646212720897490d700d0e5a40e62dc54a6c0
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-05 17:58:00 +0300

    Add test verifying that SUT doesn't clean cache upon initialisation

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -5,4 +5,24 @@
 //  Created by Stoyan Kostov on 5.06.23.
 //
 
-import Foundation
+import XCTest
+
+final class LocalFeedLoader {
+    init(store: FeedStore) {
+
+    }
+}
+
+final class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+final class CacheFeedUseCaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+}
