8a114301ddad9a69a5d5807e573873d6abbebcc7
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-05 16:30:01 +0300

    Use ephemeral URL session configuration to avoid sharing state across test executions (in-disk cache artefacts).

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -38,7 +38,7 @@ private extension EssentialFeedAPIEndToEndTests {
 
     func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
 
         trackForMemoryLeaks(client, file: file, line: line)
