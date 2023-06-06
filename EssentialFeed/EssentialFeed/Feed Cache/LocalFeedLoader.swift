2db2b2c00c65d7a12558ed38ca06b3fed2278a39
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-06 16:24:31 +0300

    Extract completion result as an typealias, this will minimise the risk of breaking changes if the result type changes

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -12,6 +12,7 @@ public final class LocalFeedLoader {
     private let currentDate: () -> Date
 
     public typealias SaveResult = Error?
+    public typealias LoadResult = LoadFeedResult
 
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -29,8 +30,12 @@ public final class LocalFeedLoader {
         })
     }
     
-    public func load(completion: @escaping (Error?) -> Void) {
-        store.retrieve(completion: completion)
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve(completion: { error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        })
     }
     
     private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
