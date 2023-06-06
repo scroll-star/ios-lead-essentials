9457c9efddc24cc2b27ca1df2eed405db4925bf8
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-06 16:41:19 +0300

    Extract locale members as properties

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -10,9 +10,14 @@ import Foundation
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
+    private let calendar = Calendar(identifier: .gregorian)
 
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
+
+    private var maxCacheAgeInDays: Int {
+        7
+    }
 
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -53,7 +58,7 @@ public final class LocalFeedLoader {
 
     private func validate(_ timestamp: Date) -> Bool {
         let calendar = Calendar(identifier: .gregorian)
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
         return currentDate() < maxCacheAge
