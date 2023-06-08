db1c7ead0b396d428aee516de4d96e01ee080023
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-08 19:08:48 +0300

    Clarify test `setUp` and `tearDown` intent by extracting clean up side effects code into helper methods

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -66,13 +66,13 @@ final class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        setupEmptyStoreState()
     }
 
     override func tearDown() {
         super.tearDown()
 
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        undoStoreSideEffects()
     }
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -155,5 +155,17 @@ final class CodableFeedStoreTests: XCTestCase {
             for: .cachesDirectory,
             in: .userDomainMask
         ).first!.appendingPathComponent("\(type(of: self)).store")
+    }
+
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 }
