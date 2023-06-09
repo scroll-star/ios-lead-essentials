cddb85f3e103a57985efe6c08c47a0094029c513
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-09 10:23:53 +0300

    Rename test method to follow convension

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -83,7 +83,7 @@ final class CodableFeedStoreTests: XCTestCase {
         expect(makeSUT(), toRetrieveTwice: .empty)
     }
 
-    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
         let sut = makeSUT()
         let feed = uniqueImageFeed().local
         let timestamp = Date()
