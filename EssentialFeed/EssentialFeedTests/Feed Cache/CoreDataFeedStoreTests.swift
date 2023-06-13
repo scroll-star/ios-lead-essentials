adfadcf5fb9654963e9d3c9920162c3aa1110022
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-13 14:07:32 +0300

    Verify store side effects run serially

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -77,7 +77,9 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
 
     func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
 
+        assertThatSideEffectsRunSerially(on: sut)
     }
 }
 
