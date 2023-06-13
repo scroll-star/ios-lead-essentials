d55c9d510728815f96cd8721e84f815ef418a092
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-13 13:36:10 +0300

    `insert` command delivers no error on empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
@@ -35,6 +35,12 @@ extension FeedStoreSpecs where Self: XCTestCase {
 
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp), file: file, line: line)
     }
+
+    func assertThatInsertDeliversNoErrorOnEmptyCache(on sut: FeedStore, file: StaticString = #filePath, line: UInt = #line) {
+        let insertionError = insert((uniqueImageFeed().local, Date()), to: sut)
+
+        XCTAssertNil(insertionError, "Expected to insert cache successfully", file: file, line: line)
+    }
 }
 
 extension FeedStoreSpecs where Self: XCTestCase {
