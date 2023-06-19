e9dfd439d6a582c145d05d7a06367807e4e5b704
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-19 13:59:17 +0300

    Render prototype feed data on screen

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -14,12 +14,28 @@ struct FeedImageViewModel {
 }
 
 final class FeedViewController: UITableViewController {
+    private let feed = FeedImageViewModel.prototypeFeed
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        feed.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
+        let model = feed[indexPath.row]
+        cell.configure(with: model)
+        return cell
+    }
+}
+
+private extension FeedImageCell {
+    func configure(with model: FeedImageViewModel) {
+        locationLabel.text = model.location
+        locationContainer.isHidden = model.location == nil
+
+        descriptionLabel.text = model.description
+        descriptionLabel.isHidden = model.description == nil
+
+        feedImageView.image = UIImage(named: model.imageName)
     }
 }
