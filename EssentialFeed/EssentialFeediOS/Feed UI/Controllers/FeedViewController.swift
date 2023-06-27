d37aa58a350f2eb6740079f08873b8a0855353c3
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-27 17:23:10 +0300

    Set `FeedViewController` title

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -22,6 +22,8 @@ public final class FeedViewController: UITableViewController, FeedLoadingView {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
+        title = "My Feed"
+
         refresh()
     }
 
