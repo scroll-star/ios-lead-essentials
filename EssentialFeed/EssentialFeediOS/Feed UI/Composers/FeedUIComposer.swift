18b1b497283a8433c1b8832efefcfa63fb0a4663
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-27 15:51:43 +0300

    Instantiate `FeedViewController` from `Feed` storyboard

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -13,7 +13,11 @@ public enum FeedUIComposer {
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
-        let feedController = FeedViewController(refreshController: refreshController)
+
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+        feedController.refreshController = refreshController
 
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(
--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 public final class FeedViewController: UITableViewController {
-    private var refreshController: FeedRefreshViewController?
+    var refreshController: FeedRefreshViewController?
     private var imageLoader: FeedImageDataLoader?
 
     var tableModel = [FeedImageCellController]() {
@@ -16,11 +16,6 @@ public final class FeedViewController: UITableViewController {
     }
 
     private var cellControllers = [IndexPath: FeedImageCellController]()
-
-    convenience init(refreshController: FeedRefreshViewController) {
-        self.init()
-        self.refreshController = refreshController
-    }
 
     public override func viewDidLoad() {
         super.viewDidLoad()
