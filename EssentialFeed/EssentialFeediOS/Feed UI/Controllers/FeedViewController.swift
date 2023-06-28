ee16dfb406394f614acf7fbcea77eb8602670af9
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-28 18:19:12 +0300

    Move `ErrorView` to separate file

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -11,10 +11,6 @@ protocol FeedViewControllerDelegate: AnyObject {
     func didRequestFeedRefresh()
 }
 
-public final class ErrorView: UIView {
-    public var message: String?
-}
-
 public final class FeedViewController: UITableViewController, FeedLoadingView {
     var delegate: FeedViewControllerDelegate?
 
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Stoyan Kostov on 28.06.23.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
