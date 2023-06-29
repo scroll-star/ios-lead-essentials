0b97e7efefa8f8a50f5882cb6539588c73552824
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-06-29 18:17:24 +0300

    `RemoteFeedImageDataLoader` requests data from URL on `loadImageDataFromURL`

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -9,8 +9,14 @@ import XCTest
 import EssentialFeed
 
 final class RemoteFeedImageDataLoader {
-    init(client: Any) {
+    private let client: HTTPClient
 
+    init(client: HTTPClient) {
+        self.client = client
+    }
+
+    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
+        client.get(from: url) { _ in }
     }
 }
 
@@ -21,13 +27,26 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
 
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
+
+    func test_loadImageDataFromURL_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
 }
 
 // MARK: - Helpers (private)
 
 private extension RemoteFeedImageDataLoaderTests {
 
-    func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+    func makeSUT(
+        url: URL = anyURL(),
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedImageDataLoader(client: client)
         trackForMemoryLeaks(sut, file: file, line: line)
@@ -35,7 +54,11 @@ private extension RemoteFeedImageDataLoaderTests {
         return (sut, client)
     }
 
-    final class HTTPClientSpy {
+    final class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            requestedURLs.append(url)
+        }
     }
 }
