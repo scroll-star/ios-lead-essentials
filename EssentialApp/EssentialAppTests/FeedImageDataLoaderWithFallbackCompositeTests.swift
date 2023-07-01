667987adb371806f7de18cd84c8e4c1b33655d84
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-07-01 14:56:29 +0300

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` loads from primary loader first.

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -14,10 +14,14 @@ final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
         }
     }
 
+    private let primary: FeedImageDataLoader
+
     init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primary = primary
     }
 
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        _ = primary.loadImageData(from: url) { _ in }
         return Task()
     }
 }
@@ -32,11 +36,27 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
+
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+
+        _ = sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
 }
 
 // MARK: - Helpers (private)
 
 private extension FeedImageDataLoaderWithFallbackCompositeTests {
+
+    func anyURL() -> URL {
+        URL(string: "http://a-url.com")!
+    }
 
     final class LoaderSpy: FeedImageDataLoader {
         private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
