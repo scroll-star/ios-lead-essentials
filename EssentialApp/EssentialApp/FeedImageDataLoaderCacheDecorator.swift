f193a1da22682ce1484982293c426833140631fa
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-07-02 13:47:19 +0300

    Create `saveIgnoringResult` method to clarify intent.

--- EssentialApp/EssentialApp/FeedImageDataLoaderCacheDecorator.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderCacheDecorator.swift
@@ -22,9 +22,15 @@ public final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
     ) -> FeedImageDataLoaderTask {
         decoratee.loadImageData(from: url) { [weak self] result in
             completion(result.map { data in
-                self?.cache.save(data, for: url) { _ in }
+                self?.cache.saveIgnoringResult(data, for: url)
                 return data
             })
         }
+    }
+}
+
+private extension FeedImageDataCache {
+    func saveIgnoringResult(_ data: Data, for url: URL) {
+        save(data, for: url) { _ in }
     }
 }
