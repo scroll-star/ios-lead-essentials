137853241a22be354f54b0ff79f08e8697c50c0f
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-07-02 14:26:51 +0300

    Rename UI tests file and class to `EssentialAppUIAcceptanceTests` which sets the foundation of UIAcceptance Tests.
    Add new xcscheme to execute these tests in isolation. Add UITests target to CI_IOS scheme in order to be able to execute them alongside the others on the CI build machine.

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialAppUITests.swift
+//  EssentialAppUITests
+//
+//  Created by Stoyan Kostov on 1.07.23.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+
+
+
+}
