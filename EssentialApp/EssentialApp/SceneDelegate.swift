6a4e395c0597eb6e95074353748cf8691162404d
scroll-star <75090675+scroll-star@users.noreply.github.com>
2023-07-06 18:02:07 +0300

    iOS 14 update: Hold reference to `RemoteFeedLoader` instance to avoid premature deallocation.
    Since iOS 14, if we don't explicitly hold a reference to the `RemoteFeedLoader` instance, it'll be deallocated before it completes the operation - so the feed will never load. That's because the Combine publisher won't hold a reference to it anymore (it used to hold the reference in iOS 13).

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -29,6 +29,11 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         LocalFeedLoader(store: store, currentDate: Date.init)
     }()
 
+    private lazy var remoteFeedLoader = {
+        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+        return RemoteFeedLoader(url: remoteURL, client: httpClient)
+    }()
+
     convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
         self.init()
         self.httpClient = httpClient
@@ -55,11 +60,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     private func makeRemoteFeedLoaderWithLocalFallback() -> FeedLoader.Publisher {
-        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
-
-        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
-
-        return remoteFeedLoader
+        remoteFeedLoader
             .loadPublisher()
             .caching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)
